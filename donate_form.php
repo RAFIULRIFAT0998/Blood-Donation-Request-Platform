d8c0e0267e5ac6af3928f51d557ec2b7acdb38d2
RAFIULRIFAT0998 <bari241-35-169@diu.edu.bd>
2025-12-15 17:15:34 +0600

    Create donate_form.php

---
+++ donate_form.php
@@ -0,0 +1,19 @@
+<div class="box">
+<h3>Become a Blood Donor</h3>
+
+<form id="donorForm">
+<input name="name" placeholder="Your Name" required>
+<input name="phone" placeholder="Phone Number" required>
+
+<select name="blood" required>
+<option value="">Select Blood Group</option>
+<option>A+</option><option>A-</option>
+<option>B+</option><option>B-</option>
+<option>O+</option><option>O-</option>
+<option>AB+</option><option>AB-</option>
+</select>
+
+<textarea name="message" placeholder="Optional message"></textarea>
+<button type="submit">Donate Blood</button>
+</form>
+</div>
