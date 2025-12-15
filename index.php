0995c059c9d6a253b6b8620f399d1f7d5467b518
RAFIULRIFAT0998 <bari241-35-169@diu.edu.bd>
2025-12-15 17:21:06 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include("includes/header.php"); ?>
+
+<?php include("donor/donate_form.php"); ?>
+<?php include("request/request_form.php"); ?>
+<?php include("donor/donor_list.php"); ?>
+<?php include("request/request_list.php"); ?>
+
+<?php include("includes/footer.php"); ?>
