cebe6c12ee2b3a630457cd859fa50a7e7faa43bd
RAFIULRIFAT0998 <bari241-35-169@diu.edu.bd>
2025-12-15 16:52:34 +0600

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Blood Donation System</title>
+<link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+<div class="container">
+<h2>🩸 Blood Donation & Request System</h2>
